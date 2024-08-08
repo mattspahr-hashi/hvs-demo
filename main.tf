4c0bd8fe3bc9665992ef79d69887649d1ee280fe
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2024-08-08 15:14:33 -0500

    added hcp creds to tfe

--- main.tf
+++ main.tf
@@ -7,6 +7,24 @@ resource "tfe_workspace" "tfc_workspace" {
   project_id = tfe_project.tfc_project.id
 }
 
+resource "hcp_service_principal" "github-actions-sp" {
+  name = "github-actions"
+}
+
+resource "hcp_service_principal_key" "key" {
+  service_principal = hcp_service_principal.github-actions-sp.name
+}
+
+resource "tfe_variable_set" "var_set" {
+  name         = "HCP Credentials"
+  organization = "mattspahr-sandbox"
+}
+
+resource "tfe_workspace_variable_set" "service_principal_var_set" {
+  workspace_id    = tfe_workspace.tfc_workspace.id
+  variable_set_id = tfe_variable_set.var_set.id
+}
+
 resource "random_string" "random" {
   length  = 16
   special = true
