3b14dced396b9a9cf00438715058ef59d4b2ed98
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2024-08-08 15:41:10 -0500

    sensitive creds

--- main.tf
+++ main.tf
@@ -8,21 +8,40 @@ resource "tfe_workspace" "tfc_workspace" {
 }
 
 resource "hcp_service_principal" "github-actions-sp" {
-  name = "github-actions"
+  name   = "github-actions"
+  parent = hcp_project.project.resource_name
 }
 
 resource "hcp_service_principal_key" "key" {
-  service_principal = hcp_service_principal.github-actions-sp.name
+  service_principal = hcp_service_principal.github-actions-sp.resource_name
 }
 
-resource "tfe_variable_set" "var_set" {
+resource "tfe_variable_set" "hcp_credentials" {
   name         = "HCP Credentials"
   organization = "mattspahr-sandbox"
 }
 
 resource "tfe_workspace_variable_set" "service_principal_var_set" {
   workspace_id    = tfe_workspace.tfc_workspace.id
-  variable_set_id = tfe_variable_set.var_set.id
+  variable_set_id = tfe_variable_set.hcp_credentials.id
+}
+
+resource "tfe_variable" "hcp_client_id" {
+  key             = "HCP_CLIENT_ID"
+  value           = hcp_service_principal_key.key.client_id
+  category        = "terraform"
+  description     = "a useful description"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.hcp_credentials.id
+}
+
+resource "tfe_variable" "hcp_client_secret" {
+  key             = "HCP_CLIENT_SECRET"
+  value           = hcp_service_principal_key.key.client_secret
+  category        = "terraform"
+  description     = "a useful description"
+  sensitive       = true
+  variable_set_id = tfe_variable_set.hcp_credentials.id
 }
 
 resource "random_string" "random" {
