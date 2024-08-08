7264aa9c6617aeba9f4097210fa8b1c6c52dd48a
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2024-08-08 15:53:57 -0500

    env variables

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "tfe_workspace_variable_set" "service_principal_var_set" {
 resource "tfe_variable" "hcp_client_id" {
   key             = "HCP_CLIENT_ID"
   value           = hcp_service_principal_key.key.client_id
-  category        = "terraform"
+  category        = "env"
   description     = "a useful description"
   sensitive       = true
   variable_set_id = tfe_variable_set.hcp_credentials.id
@@ -38,7 +38,7 @@ resource "tfe_variable" "hcp_client_id" {
 resource "tfe_variable" "hcp_client_secret" {
   key             = "HCP_CLIENT_SECRET"
   value           = hcp_service_principal_key.key.client_secret
-  category        = "terraform"
+  category        = "env"
   description     = "a useful description"
   sensitive       = true
   variable_set_id = tfe_variable_set.hcp_credentials.id
